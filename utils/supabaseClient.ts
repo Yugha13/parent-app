6c16bc864eaf9141450f406f67836c51f0fc2066
S-Axhwin <ashwin.sathiyamoorthy.d@gmail.com>
2025-09-26 11:58:47 +0530

    complted

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://gfugbqauosdgrgeqtdnd.supabase.co';
-const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmdWdicWF1b3NkZ3JnZXF0ZG5kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MTc3MjksImV4cCI6MjA3MjM5MzcyOX0.2_m34xeupYL1GKi4gzand4FnrKfqIgoPOHodiNsanMs';
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://ckelqxeojehdrfatjqtd.supabase.co';
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrZWxxeGVvamVoZHJmYXRqcXRkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1ODgwNzY0NSwiZXhwIjoyMDc0MzgzNjQ1fQ.UR9BWriS2Ud7P9p4ZNfak9WMnB-aYExTx1cRMQ7VEAI';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
